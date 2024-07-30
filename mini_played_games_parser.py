cc3cfebf86e7bb9e258227e8df89d416339bdab6
gil9red <ilya.petrash@inbox.ru>
2024-07-30 17:27:43 +0500

    Updated

--- mini_played_games_parser.py
+++ mini_played_games_parser.py
@@ -1,40 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import re
 
-from typing import Dict, List
-from urllib.parse import urljoin
-
-import requests
-
 
 # Регулярка вытаскивает выражения вида: 1, 2, 3 или 1-3, или римские цифры: III, IV
 PARSE_GAME_NAME_PATTERN = re.compile(
-    r'(\d+(, *?\d+)+)|(\d+ *?- *?\d+)|([MDCLXVI]+(, ?[MDCLXVI]+)+)',
-    flags=re.IGNORECASE
+    r"(\d+(, *?\d+)+)|(\d+ *?- *?\d+)|([MDCLXVI]+(, ?[MDCLXVI]+)+)",
+    flags=re.IGNORECASE,
 )
 
-FINISHED_GAME = 'FINISHED_GAME'
-NOT_FINISHED_GAME = 'NOT_FINISHED_GAME'
-FINISHED_WATCHED = 'FINISHED_WATCHED'
-NOT_FINISHED_WATCHED = 'NOT_FINISHED_WATCHED'
+FINISHED_GAME = "FINISHED_GAME"
+NOT_FINISHED_GAME = "NOT_FINISHED_GAME"
+FINISHED_WATCHED = "FINISHED_WATCHED"
+NOT_FINISHED_WATCHED = "NOT_FINISHED_WATCHED"
 
-FLAG_BY_CATEGORY = {
-    '  ': FINISHED_GAME,
-    '- ': NOT_FINISHED_GAME,
-    ' -': NOT_FINISHED_GAME,
-    ' @': FINISHED_WATCHED,
-    '@ ': FINISHED_WATCHED,
-    '-@': NOT_FINISHED_WATCHED,
-    '@-': NOT_FINISHED_WATCHED,
+FLAG_BY_CATEGORY: dict[str, str] = {
+    "  ": FINISHED_GAME,
+    "- ": NOT_FINISHED_GAME,
+    " -": NOT_FINISHED_GAME,
+    " @": FINISHED_WATCHED,
+    "@ ": FINISHED_WATCHED,
+    "-@": NOT_FINISHED_WATCHED,
+    "@-": NOT_FINISHED_WATCHED,
 }
 
 
-def parse_game_name(game_name: str) -> List[str]:
+def parse_game_name(game_name: str) -> list[str]:
     """
     Функция принимает название игры и пытается разобрать его, после возвращает список названий.
     У некоторых игр в названии может указываться ее части или диапазон частей, поэтому для правильного
@@ -58,14 +53,14 @@ def parse_game_name(game_name: str) -> List[str]:
     index = game_name.index(seq_str)
     base_name = game_name[:index].strip()
 
-    seq_str = seq_str.replace(' ', '')
+    seq_str = seq_str.replace(" ", "")
 
-    if ',' in seq_str:
+    if "," in seq_str:
         # '1,2,3' -> ['1', '2', '3']
-        seq = seq_str.split(',')
+        seq = seq_str.split(",")
 
-    elif '-' in seq_str:
-        seq = seq_str.split('-')
+    elif "-" in seq_str:
+        seq = seq_str.split("-")
 
         # ['1', '7'] -> [1, 7]
         seq = list(map(int, seq))
@@ -77,16 +72,28 @@ def parse_game_name(game_name: str) -> List[str]:
         return [game_name]
 
     # Сразу проверяем номер игры в серии и если она первая, то не добавляем в названии ее номер
-    return [base_name if num == '1' else base_name + " " + num for num in seq]
+    return [base_name if num == "1" else f"{base_name} {num}" for num in seq]
 
 
-def parse_played_games(text: str, silence: bool = False) -> Dict[str, Dict[str, List[str]]]:
+def parse_played_games(
+    text: str,
+    silence: bool = False,
+    errors: list[str] | None = None,
+) -> dict[str, dict[str, list[str]]]:
     """
     Функция для парсинга списка игр.
 
     """
 
-    platforms = dict()
+    if errors is None:
+        errors = []
+
+    def _process_error(error_text: str):
+        errors.append(error_text)
+        if not silence:
+            print(error_text)
+
+    platforms: dict[str, dict[str, list[str]]] = dict()
     platform = None
 
     for line in text.splitlines():
@@ -95,7 +102,7 @@ def parse_played_games(text: str, silence: bool = False) -> Dict[str, Dict[str, 
             continue
 
         flag = line[:2]
-        if flag not in FLAG_BY_CATEGORY and line.endswith(':'):
+        if flag not in FLAG_BY_CATEGORY and line.endswith(":"):
             platform_name = line[:-1]
 
             platform = {
@@ -113,8 +120,7 @@ def parse_played_games(text: str, silence: bool = False) -> Dict[str, Dict[str, 
 
         category_name = FLAG_BY_CATEGORY.get(flag)
         if not category_name:
-            if not silence:
-                print(f'Странный формат строки: "{line}"')
+            _process_error(f"Странный формат строки: {line!r}")
             continue
 
         category = platform[category_name]
@@ -122,63 +128,110 @@ def parse_played_games(text: str, silence: bool = False) -> Dict[str, Dict[str, 
         game_name = line[2:]
         for game in parse_game_name(game_name):
             if game in category:
-                if not silence:
-                    print(f'Предотвращено добавление дубликата игры "{game}"')
+                _process_error(f'Предотвращено добавление дубликата игры "{game}"')
                 continue
 
             category.append(game)
 
+    for platform, categories in platforms.items():
+        for game in categories[NOT_FINISHED_GAME]:
+            if game in categories[FINISHED_GAME]:
+                _process_error(
+                    f'Игра "{game}" ({platform}) присутствует и в не пройденных, и в пройденных'
+                )
+
+        for game in categories[NOT_FINISHED_WATCHED]:
+            if game in categories[FINISHED_WATCHED]:
+                _process_error(
+                    f'Игра "{game}" ({platform}) присутствует и в не просмотренных, и в просмотренных'
+                )
+
     return platforms
 
 
-def get_text_from_url() -> str:
-    url_gist = 'https://gist.github.com/gil9red/2f80a34fb601cd685353'
-    rs = requests.get(url_gist)
+if __name__ == "__main__":
+    print("Tests")
 
-    match = re.search('/gil9red/2f80a34fb601cd685353/raw/.+/gistfile1.txt', rs.text)
-    if not match:
-        return ''
+    text = """
+PC:
+  Foo 1-3
+? Bar
+  Bar
+  Bar
+- Bar
+@ Bar 2
+@-Bar 2
+    """.strip()
+    errors = []
+    platforms = parse_played_games(text, silence=True, errors=errors)
+    assert platforms["PC"]["FINISHED_GAME"] == ["Foo", "Foo 2", "Foo 3", "Bar"]
+    assert platforms["PC"]["NOT_FINISHED_GAME"] == ["Bar"]
+    assert platforms["PC"]["FINISHED_WATCHED"] == ["Bar 2"]
+    assert platforms["PC"]["NOT_FINISHED_WATCHED"] == ["Bar 2"]
+    assert errors == [
+        "Странный формат строки: '? Bar'",
+        'Предотвращено добавление дубликата игры "Bar"',
+        'Игра "Bar" (PC) присутствует и в не пройденных, и в пройденных',
+        'Игра "Bar 2" (PC) присутствует и в не просмотренных, и в просмотренных',
+    ]
 
-    url = urljoin(rs.url, match.group())
+    print("\n" + "-" * 100 + "\n")
 
-    rs = requests.get(url)
-    return rs.text
-
-
-def get_played_games() -> Dict[str, Dict[str, List[str]]]:
-    text = get_text_from_url()
-    return parse_played_games(text)
-
-
-if __name__ == '__main__':
     def print_text(text, export_to_file_name):
         platforms = parse_played_games(text)
-        print('Platforms:', len(platforms))
+        print("Platforms:", len(platforms))
 
         total_games = 0
         for categories in platforms.values():
             for games in categories.values():
                 total_games += len(games)
 
-        print('Games:', total_games)
+        print("Games:", total_games)
         print()
-        print(', '.join(platforms.keys()))
+        print(", ".join(platforms.keys()))
         print(platforms)
 
         import json
-        json.dump(platforms, open(export_to_file_name, mode='w', encoding='utf-8'), ensure_ascii=False, indent=4)
+
+        json.dump(
+            platforms,
+            open(export_to_file_name, mode="w", encoding="utf-8"),
+            ensure_ascii=False,
+            indent=4,
+        )
 
     def get_text_from_local():
-        with open('gistfile1.txt', encoding='utf-8') as f:
+        with open("gistfile1.txt", encoding="utf-8") as f:
             return f.read()
 
-    print('get_text_from_local')
+    print("get_text_from_local")
     text = get_text_from_local()
-    print_text(text, 'games_local.json')
+    print_text(text, "games_local.json")
 
-    print('\n')
-    print('get_text_from_url')
+    print("\n" + "-" * 100 + "\n")
+
+    from urllib.parse import urljoin
+    import requests
+
+    def get_text_from_url() -> str:
+        url_gist = "https://gist.github.com/gil9red/2f80a34fb601cd685353"
+        rs = requests.get(url_gist)
+
+        match = re.search("/gil9red/2f80a34fb601cd685353/raw/.+/gistfile1.txt", rs.text)
+        if not match:
+            return ""
+
+        url = urljoin(rs.url, match.group())
+
+        rs = requests.get(url)
+        return rs.text
+
+    def get_played_games() -> dict[str, dict[str, list[str]]]:
+        text = get_text_from_url()
+        return parse_played_games(text)
+
+    print("get_text_from_url")
     text = get_text_from_url()
-    print_text(text, 'games_url.json')
+    print_text(text, "games_url.json")
 
     assert parse_played_games(text) == get_played_games()
